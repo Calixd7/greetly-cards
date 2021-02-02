8b48f51562eb9525b43f4ed53dc44b09aad02d25
jesseyproctor <jss.proctor@gmail.com>
2021-02-02 09:45:45 -0500

    make ajax request for login

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,3 +1,15 @@
 import axios from 'axios'
 
 const API = axios.create({
+  baseURL: 'http://localhost:8000/api/auth'
+})
+
+export function login (username, password) {
+  return API
+    .post('users/', { // using Mokoon
+      username: username,
+      password: password
+    })
+    .then(result => result.data)
+  console.log(data)
+}

5f89e740fa15c724734f37a527e35e3b23fb73a3
Calixd7 <calixd7@gmail.com>
2021-02-04 13:32:01 -0500

    comma

--- backend/core/models.py
+++ backend/core/models.py
@@ -9,7 +9,7 @@ class User(AbstractUser):
 class Card(models.Model):
     author = models.ForeignKey(to=User,on_delete=models.CASCADE,related_name="cards" )
     title = models.CharField(max_length=255, blank=True, null=True)
-    message = models.TextField(blank=True null=True)
+    message = models.TextField(blank=True, null=True)
     public = models.BooleanField(default=True)
     genre = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)

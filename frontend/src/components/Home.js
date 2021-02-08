8e9768867dfafbe8c0b0a826130994821dd2664f
jesseyproctor <jss.proctor@gmail.com>
2021-02-08 13:47:56 -0500

    fix reroute for login and reg links on homepage" "

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -10,7 +10,20 @@ function Home ({ isLoggedIn }) {
       <p className='p1'>Design custom greeting cards in minutes, using our professionaly designed card templates.</p>
       <img className='image2-hp-card' src={gratefulCard} alt='gratefulCard' width='400' height='300' />
       <p className='p2'>Add images, styles and greetings to craft your perfect message.</p>
-      <p className='p3'><Link to='/login'>Log in</Link> or <Link to='/register'>Register</Link> to get started! Then...</p>
+      {/* <p className='p3'><Link to='/login'>Log in</Link> or <Link to='/register'>Register</Link> to get started! Then...</p> */}
+      <div>
+        {isLoggedIn
+          ? (
+            <span>
+              <p className='p3'><Link to='/card-list'>Log in</Link> or <Link to='/card-list'>Register</Link> to get started! Then...</p>
+            </span>
+            )
+          : (
+            <span>
+              <p className='p3'><Link to='/login'>Log in</Link> or <Link to='/register'>Register</Link> to get started! Then...</p>
+            </span>
+            )}
+      </div>
       <p className='p4'><span className='number'>1</span> Navigate to the Create page and design your custom greeting card. </p>
       <p className='p5'><span className='number'>2</span> Save your card.  Share it or keep it to yourself.</p>
       <p className='p6'><span className='number'>3</span> Explore other user's cards and connect with your favorite creative greeting card makers.</p>
@@ -27,7 +40,6 @@ function Home ({ isLoggedIn }) {
                 <Link to='/register'><button className='button-style'>Create a Card</button></Link>
               </span>
               )}
-          {/* <Link to='/register'><button className='button-style'>Create a Card</button></Link> */}
         </div>
         {/* <img src={panaramic} alt='panaramic' className='image-styling' /> */}
         <img src={getWellCard} alt='getWellCard' className='image-styling' />

7f39606c40a9b0b92fde5c1aa32bc39a9b034010
jesseyproctor <jss.proctor@gmail.com>
2021-02-07 21:34:03 -0500

    i dont know, it looks better maybe

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,4 +1,5 @@
-
+import holidayCard from '../images/holidayCard.jpg'
+import panaramic from '../images/panaramic.jpg'
 import getWellCard from '../images/getWellCard.jpg'
 import { Link } from 'react-router-dom'
 
@@ -7,16 +8,16 @@ function Home () {
     <div className='home-page-container'>
       <div className='background' />
       <h2 className='homepage-header'>Welcome to Greetly</h2>
-      <p className='p1'>Design custom greeting cards for any occasion.</p>
+      <p className='p1'>Design custom greeting cards in minutes, using our professionaly designed card templates.</p>
       {/* <img className='image-hp-card' src={getWellCard} alt='getWellCard' /> */}
       <p className='p2'>Add images, styles and greetings to craft your perfect message.</p>
-      {/* <p className='p3'><Link to='/login'>Log in</Link> or <Link to='/register'>Register</Link> to get started!</p> */}
+      <p className='p3'><Link to='/login'>Log in</Link> or <Link to='/register'>Register</Link> to get started!</p>
       <div className='image-hp-card'>
         <div className='signup-button'>
           {/* <img className='image-hp-card' src={getWellCard} alt='getWellCard' /> */}
-          <Link to='/register'><button className='button-style'>Sign up here!</button></Link>
+          <Link to='/register'><button className='button-style'>Create a Card</button></Link>
         </div>
-        <img src={getWellCard} alt='getWellCard' height='300' width='200' />
+        <img src={panaramic} alt='panaramic' className='image-styling' />
       </div>
 
     </div>

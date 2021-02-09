a18241344e0c45a61dbdce6b5ea3533d0beb6064
Calixd7 <calixd7@gmail.com>
2021-02-09 14:54:34 -0500

    page number pagination

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -156,9 +156,8 @@ REST_FRAMEWORK = {
          'rest_framework.permissions.IsAuthenticated',
     ],
 
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10
-    
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 50    
 }
 CORS_ALLOW_ALL_ORIGINS = True
 

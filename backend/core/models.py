3ee726e479560dfe181bf4d6d4e27976921c028e
Calixd7 <calixd7@gmail.com>
2021-02-10 15:38:51 -0500

    following user id change

--- backend/core/models.py
+++ backend/core/models.py
@@ -8,26 +8,6 @@ access_options = (('private','private'),
 
 class User(AbstractUser):
     name= models.CharField(max_length=255)
-  
-class Card(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE,related_name="cards" )
-    following_user_id = models.ForeignKey(to=UserFolowing, on_delete=models.CASCADE, related_name='posts')
-    message = models.TextField(blank=True, null=True)
-    size = models.CharField(max_length=100,  default="", null =True)
-    color = models.CharField(max_length=100,  blank=True, null=True)
-    style = models.CharField(max_length=100, default="", null=True)
-    font = models.CharField(max_length=100, default="", null=True)
-    weight = models.CharField(max_length=100, default="", null=True)
-    alignment = models.CharField (max_length=100, default="", null=True)
-    textboxalignment = models.CharField(max_length=100, default="", null=True)
-    image = models.URLField(blank=True, null=True)
-    access = models.CharField(max_length=100, choices=access_options, null=True)
-    genre = models.CharField(max_length=255, blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    textbackgroundopacity = models.CharField(max_length=255, blank=True, null=True)
-    backgroundopacity= models.CharField(max_length=255, blank=True, null=True)
-    backgroundcolor = models.CharField(max_length=255, blank=True, null=True)
-    textbackgroundcolor =models.CharField(max_length=255, blank=True, null=True)
 
 from django.contrib.auth import get_user_model
 UserModel = get_user_model()
@@ -48,8 +28,25 @@ class UserFollowing(models.Model):
 
         ordering = ["-created"]
 
-
-    
+class Card(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE,related_name="cards" )
+    following_user_id = models.ForeignKey(to=UserFollowing, on_delete=models.CASCADE, related_name='posts')
+    message = models.TextField(blank=True, null=True)
+    size = models.CharField(max_length=100,  default="", null =True)
+    color = models.CharField(max_length=100,  blank=True, null=True)
+    style = models.CharField(max_length=100, default="", null=True)
+    font = models.CharField(max_length=100, default="", null=True)
+    weight = models.CharField(max_length=100, default="", null=True)
+    alignment = models.CharField (max_length=100, default="", null=True)
+    textboxalignment = models.CharField(max_length=100, default="", null=True)
+    image = models.URLField(blank=True, null=True)
+    access = models.CharField(max_length=100, choices=access_options, null=True)
+    genre = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    textbackgroundopacity = models.CharField(max_length=255, blank=True, null=True)
+    backgroundopacity= models.CharField(max_length=255, blank=True, null=True)
+    backgroundcolor = models.CharField(max_length=255, blank=True, null=True)
+    textbackgroundcolor =models.CharField(max_length=255, blank=True, null=True)
 
 
 

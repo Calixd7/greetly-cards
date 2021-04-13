5928a2ec17ea5d848d87cfdbff6950ff67b171c2
calixd7 <calixd7@gmail.com>
2021-04-13 11:24:55 -0400

    changes

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -40,7 +40,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     
     'django.contrib.sites',
-    'registration',
+    # 'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
